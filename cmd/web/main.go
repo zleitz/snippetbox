a75ae4e4bd7ce76b27588e380e1f914340a6f10e
Zack Leitzel <zackleitzel@gmail.com>
2024-05-03 21:48:59 -0500

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zleitz/snippetbox
+
+go 1.22.2
